66775fa2d6c535a9badc57bd2022e2bbe1a9fd3b
aecabhijeet <arcabhijeet@gmail.com>
2025-03-14 21:22:38 -0700

    config file edited to make the skeleton.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,20 +5,20 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'AEC+ Blog',
+  tagline: 'Intelligence for Built Environment',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://blog.aec.plus',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'aecplus', // Usually your GitHub org/user name.
+  projectName: 'blog.aec.plus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -35,18 +35,23 @@ const config: Config = {
     [
       'classic',
       {
-        docs: {
+        docs: false /*{
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        }*/,
         blog: {
+          routeBasePath: '/',
+          blogTitle: 'AEC Plus Blog',
+          blogDescription: 'A blog about architecture, engineering, and construction',
           showReadingTime: true,
+          postsPerPage: 5,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: 'all', ///['rss', 'atom'],
             xslt: true,
+            copyright: `Copyright © ${new Date().getFullYear()} AEC Plus`,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -67,31 +72,35 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: false,
+    },
     navbar: {
-      title: 'My Site',
+      title: 'Blog',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'AEC Plus Logo',
         src: 'img/logo.svg',
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+          href: 'https://aec.plus',
+          label: 'AEC+',
+          position: 'right',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
+        ///{to: '/blog', label: 'Blog', position: 'left'},
+        /*{
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
           position: 'right',
-        },
+        },*/
       ],
     },
     footer: {
       style: 'dark',
       links: [
-        {
+        /*{
           title: 'Docs',
           items: [
             {
@@ -103,35 +112,24 @@ const config: Config = {
         {
           title: 'Community',
           items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
+
             {
               label: 'X',
-              href: 'https://x.com/docusaurus',
+              href: 'https://x.com/aecplus',
             },
           ],
-        },
+        },*/
         {
-          title: 'More',
+          title: 'Quicklinks',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: 'AEC Plus',
+              href: 'https://aec.plus',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} AEC+`,
     },
     prism: {
       theme: prismThemes.github,
